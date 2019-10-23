4a0ce3f89d59f91b15bb96dd1e1c0e6eeea2f4ae
Dylan Vann <dylan@dylanvann.com>
2019-10-23 16:07:35 -0400

    Bundle dependencies.

--- dist/jira-check.esm.js
+++ dist/jira-check.esm.js
@@ -1,4 +1,388 @@
-import { setFailed } from '@actions/core';
+import os from 'os';
+import path from 'path';
 
-setFailed('It did not work.');
+var commonjsGlobal = typeof globalThis !== 'undefined' ? globalThis : typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
+
+function unwrapExports (x) {
+	return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, 'default') ? x['default'] : x;
+}
+
+function createCommonjsModule(fn, module) {
+	return module = { exports: {} }, fn(module, module.exports), module.exports;
+}
+
+var command = createCommonjsModule(function (module, exports) {
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+
+/**
+ * Commands
+ *
+ * Command Format:
+ *   ##[name key=value;key=value]message
+ *
+ * Examples:
+ *   ##[warning]This is the user warning message
+ *   ##[set-secret name=mypassword]definitelyNotAPassword!
+ */
+
+
+function issueCommand(command, properties, message) {
+  const cmd = new Command(command, properties, message);
+  process.stdout.write(cmd.toString() + os.EOL);
+}
+
+exports.issueCommand = issueCommand;
+
+function issue(name, message = '') {
+  issueCommand(name, {}, message);
+}
+
+exports.issue = issue;
+const CMD_STRING = '::';
+
+class Command {
+  constructor(command, properties, message) {
+    if (!command) {
+      command = 'missing.command';
+    }
+
+    this.command = command;
+    this.properties = properties;
+    this.message = message;
+  }
+
+  toString() {
+    let cmdStr = CMD_STRING + this.command;
+
+    if (this.properties && Object.keys(this.properties).length > 0) {
+      cmdStr += ' ';
+
+      for (const key in this.properties) {
+        if (this.properties.hasOwnProperty(key)) {
+          const val = this.properties[key];
+
+          if (val) {
+            // safely append the val - avoid blowing up when attempting to
+            // call .replace() if message is not a string for some reason
+            cmdStr += `${key}=${escape(`${val || ''}`)},`;
+          }
+        }
+      }
+    }
+
+    cmdStr += CMD_STRING; // safely append the message - avoid blowing up when attempting to
+    // call .replace() if message is not a string for some reason
+
+    const message = `${this.message || ''}`;
+    cmdStr += escapeData(message);
+    return cmdStr;
+  }
+
+}
+
+function escapeData(s) {
+  return s.replace(/\r/g, '%0D').replace(/\n/g, '%0A');
+}
+
+function escape(s) {
+  return s.replace(/\r/g, '%0D').replace(/\n/g, '%0A').replace(/]/g, '%5D').replace(/;/g, '%3B');
+}
+});
+
+unwrapExports(command);
+var command_1 = command.issueCommand;
+var command_2 = command.issue;
+
+var core = createCommonjsModule(function (module, exports) {
+
+var __awaiter = commonjsGlobal && commonjsGlobal.__awaiter || function (thisArg, _arguments, P, generator) {
+  function adopt(value) {
+    return value instanceof P ? value : new P(function (resolve) {
+      resolve(value);
+    });
+  }
+
+  return new (P || (P = Promise))(function (resolve, reject) {
+    function fulfilled(value) {
+      try {
+        step(generator.next(value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function rejected(value) {
+      try {
+        step(generator["throw"](value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function step(result) {
+      result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
+    }
+
+    step((generator = generator.apply(thisArg, _arguments || [])).next());
+  });
+};
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+
+
+
+
+
+/**
+ * The code to exit an action
+ */
+
+
+var ExitCode;
+
+(function (ExitCode) {
+  /**
+   * A code indicating that the action was successful
+   */
+  ExitCode[ExitCode["Success"] = 0] = "Success";
+  /**
+   * A code indicating that the action was a failure
+   */
+
+  ExitCode[ExitCode["Failure"] = 1] = "Failure";
+})(ExitCode = exports.ExitCode || (exports.ExitCode = {})); //-----------------------------------------------------------------------
+// Variables
+//-----------------------------------------------------------------------
+
+/**
+ * Sets env variable for this action and future actions in the job
+ * @param name the name of the variable to set
+ * @param val the value of the variable
+ */
+
+
+function exportVariable(name, val) {
+  process.env[name] = val;
+  command.issueCommand('set-env', {
+    name
+  }, val);
+}
+
+exports.exportVariable = exportVariable;
+/**
+ * Registers a secret which will get masked from logs
+ * @param secret value of the secret
+ */
+
+function setSecret(secret) {
+  command.issueCommand('add-mask', {}, secret);
+}
+
+exports.setSecret = setSecret;
+/**
+ * Prepends inputPath to the PATH (for this action and future actions)
+ * @param inputPath
+ */
+
+function addPath(inputPath) {
+  command.issueCommand('add-path', {}, inputPath);
+  process.env['PATH'] = `${inputPath}${path.delimiter}${process.env['PATH']}`;
+}
+
+exports.addPath = addPath;
+/**
+ * Gets the value of an input.  The value is also trimmed.
+ *
+ * @param     name     name of the input to get
+ * @param     options  optional. See InputOptions.
+ * @returns   string
+ */
+
+function getInput(name, options) {
+  const val = process.env[`INPUT_${name.replace(/ /g, '_').toUpperCase()}`] || '';
+
+  if (options && options.required && !val) {
+    throw new Error(`Input required and not supplied: ${name}`);
+  }
+
+  return val.trim();
+}
+
+exports.getInput = getInput;
+/**
+ * Sets the value of an output.
+ *
+ * @param     name     name of the output to set
+ * @param     value    value to store
+ */
+
+function setOutput(name, value) {
+  command.issueCommand('set-output', {
+    name
+  }, value);
+}
+
+exports.setOutput = setOutput; //-----------------------------------------------------------------------
+// Results
+//-----------------------------------------------------------------------
+
+/**
+ * Sets the action status to failed.
+ * When the action exits it will be with an exit code of 1
+ * @param message add error issue message
+ */
+
+function setFailed(message) {
+  process.exitCode = ExitCode.Failure;
+  error(message);
+}
+
+exports.setFailed = setFailed; //-----------------------------------------------------------------------
+// Logging Commands
+//-----------------------------------------------------------------------
+
+/**
+ * Writes debug message to user log
+ * @param message debug message
+ */
+
+function debug(message) {
+  command.issueCommand('debug', {}, message);
+}
+
+exports.debug = debug;
+/**
+ * Adds an error issue
+ * @param message error issue message
+ */
+
+function error(message) {
+  command.issue('error', message);
+}
+
+exports.error = error;
+/**
+ * Adds an warning issue
+ * @param message warning issue message
+ */
+
+function warning(message) {
+  command.issue('warning', message);
+}
+
+exports.warning = warning;
+/**
+ * Writes info to log with console.log.
+ * @param message info message
+ */
+
+function info(message) {
+  process.stdout.write(message + os.EOL);
+}
+
+exports.info = info;
+/**
+ * Begin an output group.
+ *
+ * Output until the next `groupEnd` will be foldable in this group
+ *
+ * @param name The name of the output group
+ */
+
+function startGroup(name) {
+  command.issue('group', name);
+}
+
+exports.startGroup = startGroup;
+/**
+ * End an output group.
+ */
+
+function endGroup() {
+  command.issue('endgroup');
+}
+
+exports.endGroup = endGroup;
+/**
+ * Wrap an asynchronous function call in a group.
+ *
+ * Returns the same type as the function itself.
+ *
+ * @param name The name of the group
+ * @param fn The function to wrap in the group
+ */
+
+function group(name, fn) {
+  return __awaiter(this, void 0, void 0, function* () {
+    startGroup(name);
+    let result;
+
+    try {
+      result = yield fn();
+    } finally {
+      endGroup();
+    }
+
+    return result;
+  });
+}
+
+exports.group = group; //-----------------------------------------------------------------------
+// Wrapper action state
+//-----------------------------------------------------------------------
+
+/**
+ * Saves state for current action, the state can only be retrieved by this action's post job execution.
+ *
+ * @param     name     name of the state to store
+ * @param     value    value to store
+ */
+
+function saveState(name, value) {
+  command.issueCommand('save-state', {
+    name
+  }, value);
+}
+
+exports.saveState = saveState;
+/**
+ * Gets the value of an state set by this action's main execution.
+ *
+ * @param     name     name of the state to get
+ * @returns   string
+ */
+
+function getState(name) {
+  return process.env[`STATE_${name}`] || '';
+}
+
+exports.getState = getState;
+});
+
+unwrapExports(core);
+var core_1 = core.ExitCode;
+var core_2 = core.exportVariable;
+var core_3 = core.setSecret;
+var core_4 = core.addPath;
+var core_5 = core.getInput;
+var core_6 = core.setOutput;
+var core_7 = core.setFailed;
+var core_8 = core.debug;
+var core_9 = core.error;
+var core_10 = core.warning;
+var core_11 = core.info;
+var core_12 = core.startGroup;
+var core_13 = core.endGroup;
+var core_14 = core.group;
+var core_15 = core.saveState;
+var core_16 = core.getState;
+
+core_7('It did not work.');
 //# sourceMappingURL=jira-check.esm.js.map
---
+++ tsdx.config.js
@@ -0,0 +1,20 @@
+const commonjs = require('rollup-plugin-commonjs');
+const resolve = require('rollup-plugin-node-resolve');
+
+// Not transpiled with TypeScript or Babel, so use plain Es6/Node.js!
+module.exports = {
+  target: 'node',
+  // This function will run for each entry/format/env combination
+  rollup(config, options) {
+    // We want to bundle everything except built in modules.
+    config.external = ['os', 'path'];
+    // This plugin isn't added by tsdx except in UMD mode, we need it though.
+    // https://github.com/jaredpalmer/tsdx/blob/0a7a4692f70ccb2bfccb52460d2aad6aebe093a9/src/createRollupConfig.ts#L105
+    config.plugins.push(
+      commonjs({
+        include: /\/node_modules\//,
+      })
+    );
+    return config; // always return a config.
+  },
+};
