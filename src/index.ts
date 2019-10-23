0e1c3842af384dfe4520bd90534033f887c1707f
Dylan Vann <dylan@dylanvann.com>
2019-10-23 17:30:40 -0400

    Log the context.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 
-core.setFailed('It did not work.');
+function run() {
+  // const myToken = core.getInput('repo-token');
+  // const octokit = new github.GitHub(myToken);
+  const context = github.context;
+  core.info(JSON.stringify(context, null, 2));
+  core.setFailed('It did not work.');
+}
+
+run();
