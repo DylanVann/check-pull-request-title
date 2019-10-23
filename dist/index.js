3a101d8dec367d24546892a2455da343d3b12330
Dylan Vann <dylan@dylanvann.com>
2019-10-23 17:40:33 -0400

    Remove dist.

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./jira-check.cjs.production.min.js')
-} else {
-  module.exports = require('./jira-check.cjs.development.js')
-}
