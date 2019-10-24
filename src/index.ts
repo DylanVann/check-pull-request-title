7700dcc7c669ae6073c7393ffb6b3c378e9d7250
Dylan Vann <dylan@dylanvann.com>
2019-10-24 01:05:41 -0400

    Make it actually work.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
+import * as core from '@actions/core'
+import * as github from '@actions/github'
 
 function run() {
-  // const myToken = core.getInput('repo-token');
-  // const octokit = new github.GitHub(myToken);
-  const context = github.context;
-  core.info(JSON.stringify(context, null, 2));
-  core.setFailed('It did not work.');
+  const pattern = core.getInput('pattern')
+  const regex = new RegExp(pattern)
+  const title =
+    github.context.payload &&
+    github.context.payload.pull_request &&
+    github.context.payload.pull_request.title
+  core.info(title)
+  const isValid = regex.test(title)
+  if (!isValid) {
+    core.setFailed(
+      `Pull request title "${title}" does not match pattern ${pattern}.`,
+    )
+  }
 }
 
-run();
+run()
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,3 +1,3 @@
 test('it runs', () => {
-  expect(2 + 2).toBe(4);
-});
+  expect(2 + 2).toBe(4)
+})
